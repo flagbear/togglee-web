76c2f59ed59a308337a686aac9d1042c55a9fa7a
kanekotic <me@kanekotic.com>
2022-07-24 11:30:19 +0200

    Alvaro | fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,14 +40,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/togglee/togglee-website/tree/main/',
+            'https://github.com/togglee/togglee-web/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/togglee/togglee-website/tree/main/',
+            'https://github.com/togglee/togglee-web/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
